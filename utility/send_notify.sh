496bcb50118dea937a2ac5f6d2a1893110682615
Zhernovkov Nikita <zhernovkov@proitr.ru>
2023-04-19 13:01:10 +0500

    add notify example

---
+++ utility/send_notify.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ 
+ curl -X POST \
+     -H 'Content-Type: application/json' \
+     -d '{"chat_id": "${CHAT_ID}", "text": "Github CI/CD message", "disable_notification": true}' \
+     https://api.telegram.org/bot1568895430:AAG22Vr73NTS_U40qzy0B_jiJVmUj6mf1Qk/sendMessage
