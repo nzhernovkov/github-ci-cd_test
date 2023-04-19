93fafb9fd8acc972c5ace915d81b1c2ff60e4063
Zhernovkov Nikita <zhernovkov@proitr.ru>
2023-04-19 13:20:13 +0500

    update script

--- utility/send_notify.sh
+++ utility/send_notify.sh
@@ -2,5 +2,5 @@
  
  curl -X POST \
      -H 'Content-Type: application/json' \
-     -d '{"chat_id": ${CHAT_ID}, "text": "Github CI/CD message", "disable_notification": true}' \
+     -d "{\"chat_id\": \"${CHAT_ID}\", \"text\": \"Github CI/CD message\", \"disable_notification\": true}" \
      https://api.telegram.org/bot1568895430:AAG22Vr73NTS_U40qzy0B_jiJVmUj6mf1Qk/sendMessage
